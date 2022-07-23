08c6c241b491cadee2c3d92ec73673379dbb80cf
mgfiorentin <matheus.fiorentin@gmail.com>
2022-07-23 08:12:05 -0300

    Listando empresas e seus ativos

--- INOA/INOA/settings.py
+++ INOA/INOA/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'InvestimentoApp.apps.InvestimentoappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- INOA/InvestimentoApp/views.py
+++ INOA/InvestimentoApp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+import requests
 
-# Create your views here.
+def index(request):
+    return HttpResponse("InvestimentoApp Index")
+
+def lista(request):
+    url= 'https://api-cotacao-b3.labdo.it/api/empresa' 
+    r = requests.get(url)
+    
+    lista_empresas = r.json()
+    
+    template = loader.get_template('investimentoapp/lista.html')
+    context = {
+        'lista_empresas' : lista_empresas,    
+    }
+    return HttpResponse(template.render(context,request))
+
+def detalhe(request, empresa_id):
+    return HttpResponse("Detalhe empresa %s" %empresa_id)
+
